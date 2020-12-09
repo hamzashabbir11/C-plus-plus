13b21e641926f563aa9cbfdc7382d8a51935148e
Hamza Shabbir <62654154+hamzashabbir11@users.noreply.github.com>
2020-12-09 18:31:07 +0500

    Array Average

--- Source.cpp
+++ Source.cpp
@@ -1,53 +1,19 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
-
-int main()
+void main()
 {
-	int r, c, a[100][100], b[100][100], sum[100][100], i, j;
+float total = 0;
+float avg;
 
-	cout << "Enter number of rows (between 1 and 100): ";
-	cin >> r;
-
-	cout << "Enter number of columns (between 1 and 100): ";
-	cin >> c;
-
-	cout << endl << "Enter elements of 1st matrix: " << endl;
-
-	// Storing elements of first matrix entered by user.
-	for (i = 0; i < r; ++i)
-		for (j = 0; j < c; ++j)
-		{
-			cout << "Enter element a" << i + 1 << j + 1 << " : ";
-			cin >> a[i][j];
-		}
-	// my first comment
-	// Storing elements of second matrix entered by user.
-	cout << endl << "Enter elements of 2nd matrix: " << endl;
-	for (i = 0; i < r; ++i)
-		for (j = 0; j < c; ++j)
-		{
-			cout << "Enter element b" << i + 1 << j + 1 << " : ";
-			cin >> b[i][j];
-		}
-	// Adding Two matrices
-	for (i = 0; i < r; ++i)
-		for (j = 0; j < c; ++j)
-			sum[i][j] = a[i][j] + b[i][j];
-	
-	// Displaying the resultant sum matrix.
-	cout << endl << "Sum of two matrix is: " << endl;
-	for (i = 0; i < r; ++i)
+	int average[5];
+	for (int i = 0; i < 5; i++)
 	{
-		for (j = 0; j < c; ++j)
-		{
-			cout << sum[i][j] << "  ";
-			
-
-		}
-		cout << endl;
+		cin >> average[i];
+		
+		total = total + average[i];
+		
+		avg = total / 5;
 	}
-	
+	cout << avg;
 	system("pause");
-	return 0;
-	
 }
