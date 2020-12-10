b542d9a0283a83fc8bc06e94ec1b9f174e31337d
Hamza Shabbir <62654154+hamzashabbir11@users.noreply.github.com>
2020-12-10 20:51:51 +0500

    Addition of Matrices

--- Source.cpp
+++ Source.cpp
@@ -1,20 +1,53 @@
-#include<iostream>
-
-#include<string>
+#include <iostream>
 using namespace std;
+
 int main()
 {
-	int  arr1[3][3] = { {},{} };
-	
-	for (int i = 0; i < 3; i++)
-	{
-		for (int j = 0; j < 3; j++)
+	int r, c, a[100][100], b[100][100], sum[100][100], i, j;
+
+	cout << "Enter number of rows (between 1 and 100): ";
+	cin >> r;
+
+	cout << "Enter number of columns (between 1 and 100): ";
+	cin >> c;
+
+	cout << endl << "Enter elements of 1st matrix: " << endl;
+
+	// Storing elements of first matrix entered by user.
+	for (i = 0; i < r; ++i)
+		for (j = 0; j < c; ++j)
 		{
-			cout << arr1[i][j] << "  ";
+			cout << "Enter element a" << i + 1 << j + 1 << " : ";
+			cin >> a[i][j];
+		}
+	// my first comment
+	// Storing elements of second matrix entered by user.
+	cout << endl << "Enter elements of 2nd matrix: " << endl;
+	for (i = 0; i < r; ++i)
+		for (j = 0; j < c; ++j)
+		{
+			cout << "Enter element b" << i + 1 << j + 1 << " : ";
+			cin >> b[i][j];
+		}
+	// Adding Two matrices
+	for (i = 0; i < r; ++i)
+		for (j = 0; j < c; ++j)
+			sum[i][j] = a[i][j] + b[i][j];
+	
+	// Displaying the resultant sum matrix.
+	cout << endl << "Sum of two matrix is: " << endl;
+	for (i = 0; i < r; ++i)
+	{
+		for (j = 0; j < c; ++j)
+		{
+			cout << sum[i][j] << "  ";
+			
+
 		}
 		cout << endl;
 	}
-		
-		system("pause");
-		return 0;
+	
+	system("pause");
+	return 0;
+	
 }
